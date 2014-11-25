54ca4a8cf40f56346c2abf7b96042f3bccc77a8a
LFDM <1986gh@gmail.com>
2014-11-25 23:10:18 +0100

    Add more spec examples

--- spec/treebank/transform_spec.rb
+++ spec/treebank/transform_spec.rb
@@ -25,6 +25,86 @@ describe Treebank::Transform do
 EOF
   end
 
+  let(:tb2) do
+<<EOF
+<treebank>
+  <sentence id="126" document_id="Perseus:text:1999.02.0010" subdoc="text=Catil.:Speech=1:chapter=7" span="si4:desinam0">
+    <word id="1" form="si" lemma="si1" postag="c--------" head="6" relation="AuxC"/>
+    <word id="2" form="est" lemma="sum1" postag="v3spia---" head="1" relation="ADV"/>
+    <word id="3" form="verus" lemma="verus1" postag="a-s---mn-" head="2" relation="PNOM"/>
+    <word id="4" form="," lemma="comma1" postag="u--------" head="1" relation="AuxX"/>
+    <word id="5" form="ne" lemma="ne1" postag="c--------" head="7" relation="AuxC"/>
+    <word id="6" form="opprimar" lemma="opprimo1" postag="v1spsp---" head="5" relation="ExD_CO"/>
+    <word id="7" form="," lemma="comma1" postag="u--------" head="0" relation="COORD"/>
+    <word id="8" form="sin" lemma="si1" postag="c--------" head="15" relation="AuxC"/>
+    <word id="9" form="falsus" lemma="falsus1" postag="a-s---mn-" head="8" relation="PNOM_ExD0_ADV"/>
+    <word id="10" form="," lemma="comma1" postag="u--------" head="8" relation="AuxX"/>
+    <word id="11" form="ut" lemma="ut1" postag="c--------" head="7" relation="AuxC"/>
+    <word id="12" form="tandem" lemma="tandem1" postag="d--------" head="15" relation="AuxY"/>
+    <word id="13" form="aliquando" lemma="aliquando1" postag="d--------" head="15" relation="ADV"/>
+    <word id="14" form="timere" lemma="timeo1" postag="v--pna---" head="15" relation="OBJ"/>
+    <word id="15" form="desinam" lemma="desino1" postag="v1spsa---" head="11" relation="ExD_CO"/>
+  </sentence>
+</treebank>
+EOF
+  end
+
+  let(:tb2_result) do
+<<EOF
+<treebank>
+  <sentence id="126" document_id="Perseus:text:1999.02.0010" subdoc="text=Catil.:Speech=1:chapter=7" span="si4:desinam0">
+    <word id="1" form="si" lemma="si1" postag="c--------" head="6" relation="AuxC"/>
+    <word id="2" form="est" lemma="sum1" postag="v3spia---" head="1" relation="ADV"/>
+    <word id="3" form="verus" lemma="verus1" postag="a-s---mn-" head="2" relation="PNOM"/>
+    <word id="4" form="," lemma="comma1" postag="u--------" head="1" relation="AuxX"/>
+    <word id="5" form="ne" lemma="ne1" postag="c--------" head="7" relation="AuxC"/>
+    <word id="6" form="opprimar" lemma="opprimo1" postag="v1spsp---" head="5" relation="ExD_CO"/>
+    <word id="7" form="," lemma="comma1" postag="u--------" head="0" relation="COORD"/>
+    <word id="8" form="sin" lemma="si1" postag="c--------" head="15" relation="AuxC"/>
+    <word id="9" form="falsus" lemma="falsus1" postag="a-s---mn-" head="16" relation="PNOM"/>
+    <word id="10" form="," lemma="comma1" postag="u--------" head="8" relation="AuxX"/>
+    <word id="11" form="ut" lemma="ut1" postag="c--------" head="7" relation="AuxC"/>
+    <word id="12" form="tandem" lemma="tandem1" postag="d--------" head="15" relation="AuxY"/>
+    <word id="13" form="aliquando" lemma="aliquando1" postag="d--------" head="15" relation="ADV"/>
+    <word id="14" form="timere" lemma="timeo1" postag="v--pna---" head="15" relation="OBJ"/>
+    <word id="15" form="desinam" lemma="desino1" postag="v1spsa---" head="11" relation="ExD_CO"/>
+    <word id="16" insertion_id="0016e" form="[0]" artificial="true" head="8" relation="ADV"/>
+  </sentence>
+</treebank>
+EOF
+  end
+
+  let(:tb3) do
+<<EOF
+<treebank>
+  <sentence id="95" document_id="Perseus:text:1999.02.0010" subdoc="text=Catil.:Speech=1:chapter=6" span="quid2:vero0">
+    <word id="1" form="quid" lemma="quis1" postag="p-s---nn-" head="0" relation="SBJ_ExD0_PRED"/>
+    <word id="2" form="vero" lemma="verus1" postag="d--------" head="0" relation="AuxY_ExD0_PRED"/>
+  </sentence>
+</treebank>
+EOF
+  end
+
+  let(:tb3_result) do
+<<EOF
+<treebank>
+  <sentence id="95" document_id="Perseus:text:1999.02.0010" subdoc="text=Catil.:Speech=1:chapter=6" span="quid2:vero0">
+    <word id="1" form="quid" lemma="quis1" postag="p-s---nn-" head="3" relation="SBJ"/>
+    <word id="2" form="vero" lemma="verus1" postag="d--------" head="3" relation="AuxY"/>
+    <word id="3" insertion_id="0002e" artificial="elliptic" relation="PRED" head="0" form="[0]"/>
+  </sentence>
+</treebank>
+EOF
+  end
+
+  #let(:tb4) do
+#<<EOF
+#<treebank>
+
+#</treebank>
+#EOF
+  #end
+
   describe "#transform" do
     it "returns the document when there is nothing to transform" do
       tb = Treebank::Transform.new(tb1)
