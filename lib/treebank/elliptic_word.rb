e35e0dc3c0f4c1ddb8847a985234e7f87e226693
LFDM <1986gh@gmail.com>
2014-11-26 02:19:44 +0100

    More refactoring

--- lib/treebank/elliptic_word.rb
+++ lib/treebank/elliptic_word.rb
@@ -6,15 +6,12 @@ module Treebank
     end
 
     def parse_elliptic_head
-      match = @word['relation'].match(regexp)
-      return unless match
+      return unless match = @word['relation'].match(regexp)
 
       label, elliptic_string, elliptic_label = match.captures
       elliptic_head = @word['head']
 
-      if cached_head = @sentence.elliptic_nodes[elliptic_string]
-        head = cached_head
-      else
+      unless head = @sentence.elliptic_nodes[elliptic_string]
         new_node = create_new_node(elliptic_head, elliptic_label, elliptic_string)
         head = new_node['id']
       end
